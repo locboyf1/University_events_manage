033da3603b1d2ca706520671325d65d2137d1d91
locboyf1 <locboyf1@gmail.com>
2025-10-28 17:03:00 +0700

    Thêm tính năng tạo, sửa ngành, lớp

--- src/main/java/com/event/university/repository/LopRepository.java
+++ src/main/java/com/event/university/repository/LopRepository.java
@@ -1,6 +1,7 @@
 package com.event.university.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,6 @@ import com.event.university.entity.Lop;
 
 public interface LopRepository extends JpaRepository<Lop, Integer> {
 	List<Lop> findByNganhId(Integer nganhId);
+
+	Optional<Lop> findById(Integer id);
 }
